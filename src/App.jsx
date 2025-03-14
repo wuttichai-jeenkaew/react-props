ec1ec9db9d6029b787cabd55903e2ded9090b3d2
wuttichai-jeenkaew <wuttichai.jeenkaew@gmail.com>
2025-03-14 14:10:22 +0700

    assignment-props

--- src/App.jsx
+++ src/App.jsx
@@ -51,7 +51,7 @@ function MainContent() {
           to collaborate with you to tackle your most complex challenges and
           drive your success.
         </p>
-        <Button />
+        <Button buttonText="Learn More" buttonStyle="learn-button"/>
       </div>
     </main>
   );
@@ -70,7 +70,7 @@ function ProductSection() {
             />
             <h3>Premium Bluetooth Headphones</h3>
             <p className="product-price">2,400 Baht</p>
-            <Button />
+            <Button buttonText="Add to Cart" buttonStyle="add-to-cart-button" />
           </div>
           <div className="product-card">
             <img
@@ -79,7 +79,7 @@ function ProductSection() {
             />
             <h3>Classic Leather Watch</h3>
             <p className="product-price">4,000 Baht</p>
-            <Button />
+            <Button buttonText="Add to Cart" buttonStyle="add-to-cart-button"/>
           </div>
           <div className="product-card">
             <img
@@ -88,7 +88,7 @@ function ProductSection() {
             />
             <h3>Organic Green Tea</h3>
             <p className="product-price">79.99 Baht</p>
-            <Button />
+            <Button buttonText="Add to Cart" buttonStyle="add-to-cart-button-secondary"/>
           </div>
         </div>
       </div>
@@ -104,8 +104,8 @@ function Footer() {
   );
 }
 
-function Button() {
-  return <button className="add-to-cart-button">Add to Cart</button>;
+function Button(props) {
+  return <button className={props.buttonStyle}>{props.buttonText}</button>;
 }
 
 function App() {
